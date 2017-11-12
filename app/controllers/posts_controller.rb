e66583da99883b8c3c6f2cf08a82433290269ae6
Artem Rakov <scoff1k@gmail.com>
2017-11-12 18:21:16 +0000

    Implemented edit and update functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,9 +29,9 @@ class PostsController < ApplicationController
   def update
     @post = Post.find(params[:id])
     if @post.update(post_params)
-      redirect_to 
+      redirect_to post_path(@post)
     else
-
+      render :edit
     end
   end
 
