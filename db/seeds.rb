2ca7f782cc63f6f7178950c12d01add5f2e41de2
Artem Rakov <scoff1k@gmail.com>
2017-11-16 22:05:41 +0000

    Updated audit log factory

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,20 +4,21 @@ user = User.create(first_name: "john",
                     email: 'test@test.com',
                     password: "123123",
                     password_confirmation: "123123",
-                    number: "1234567890")
+                    phone: "1234567890")
 
 
-user = AdmiUser.create(first_name: "Admin",
+user = AdminUser.create(first_name: "Admin",
                     last_name: "Test",
                     email: 'admin@test.com',
                     password: "123123",
                     password_confirmation: "123123",
-                    number: "1234567890")
+                    phone: "1234567890")
 
 puts "Admin and User was created"
 
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content", user: user, overtime_request: 2.5)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user: user, overtime_request: 2.5)
+  AuditLog.create!(user: user, start_date: (Date.today - 6.days))
 end
 
 puts "100 Post have been created"
