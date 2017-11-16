1f201b97bd15e20a11550995edf63a022dd90f4e
Artem Rakov <scoff1k@gmail.com>
2017-11-16 14:53:37 +0000

    Implemented labels with status for posts

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,18 @@
 module PostsHelper
+  def status_label(status)
+    status_span_generator(status)
+  end
+
+  private
+
+  def status_span_generator(status)
+    case status
+    when 'Submitted'
+      content_tag(:span, status, class: "label label-primary")
+    when 'Approved'
+      content_tag(:span, status, class: "label label-success")
+    when 'Rejected'
+      content_tag(:span, status, class: "label-danger")
+    end
+  end
 end
