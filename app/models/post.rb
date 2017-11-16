87ab825e587b23716409acc51db471d14b0139a4
Artem Rakov <scoff1k@gmail.com>
2017-11-16 15:45:59 +0000

    added hours requested in post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :rationale, :date, presence: true
+  validates :rationale, :date, :overtime_request, presence: true
+  validates :overtime_request, numericality: { greater_than: 0.0 }
   enum status: { submitted: 0, approved: 1, rejected: 2 }
 
   scope :posts_by, ->(user) { where(user: user) }
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,20 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  describe "Creation" do 
-    before do 
+  describe "Creation" do
+    before do
       user = FactoryBot.create(:user)
       login_as(user, :scope => :user)
       @post = FactoryBot.create(:post)
     end
-    
-    it "can be created" do 
+
+    it "can be created" do
       expect(@post).to be_valid
     end
 
-    it "can not be created without date and rationale" do 
+    it "can not be created without date, overtime_request and rationale" do
       @post.date = nil
       @post.rationale = nil
+      @post.overtime_request = nil
+      expect(@post).to_not be_valid
+    end
+
+    it 'has overtime_request greater than 0.0' do
+      @post.overtime_request = 0.0
       expect(@post).to_not be_valid
     end
   end
