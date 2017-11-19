cc383e61e5e4d71ecbebaf7153995e8fe93993b8
Artem Rakov <scoff1k@gmail.com>
2017-11-19 07:27:41 +0000

    Styled the employee homepage

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,7 +18,10 @@ puts "Admin and User was created"
 
 100.times do |post|
   Post.create!(date: Date.today, rationale: "#{post} rationale content orem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsu", user: user, overtime_request: 2.5)
-  AuditLog.create!(user: user, start_date: (Date.today - 6.days))
 end
+
+AuditLog.create!(user: user, start_date: (Date.today - 6.days))
+AuditLog.create!(user: user, start_date: (Date.today - 13.days))
+AuditLog.create!(user: user, start_date: (Date.today - 20.days))
 
 puts "100 Post have been created"
